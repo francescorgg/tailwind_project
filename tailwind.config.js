28cfd669c098ccad4ab0b88a3cd816a500b52576
francescorgg <frn.rgg@gmail.com>
2023-04-19 23:02:40 +0200

    hello world, first project in tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], //useremo './*.html' non avendo per questo progetto delle src directory o altro
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
