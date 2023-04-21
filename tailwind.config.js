e78413b2cdcb6cbfa8d18b62af01c3b8c1d7429c
francescorgg <frn.rgg@gmail.com>
2023-04-21 19:55:32 +0200

    new header under navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ['./*.html'], //useremo './*.html' non avendo per questo progetto delle src directory o altro
   theme: {
+    screens: {
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
     extend: {
       colors: {
 
         // https://coolors.co/palettes/trending <---- link per palette dei colori
         paletteRichBlack: 'rgb(47, 72, 88)',
+        paletteGrey: 'rgb(177, 167, 166)',
         paletteBlueMarine: 'rgb(51, 101, 138)',
         paletteCerulean: 'rgb(51, 101, 138)',
         paletteYellow: 'rgb(246, 174, 45)',
