7855d628d7dd55fe3abb4841ac4488676518c5fa
francescorgg <frn.rgg@gmail.com>
2023-04-21 17:23:06 +0200

    new navbar with logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./*.html'], //useremo './*.html' non avendo per questo progetto delle src directory o altro
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        // https://coolors.co/palettes/trending <---- link per palette dei colori
+        paletteRichBlack: 'rgb(47, 72, 88)',
+        paletteBlueMarine: 'rgb(51, 101, 138)',
+        paletteCerulean: 'rgb(51, 101, 138)',
+        paletteYellow: 'rgb(246, 174, 45)',
+        paletteOrange: 'rgb(242, 100, 25)'
+      }
+    },
   },
   plugins: [],
 }
